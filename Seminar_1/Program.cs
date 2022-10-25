726c69619d8dbd45db3f096d5104f9bef01dc4fa
Vladimir <grandtwix33@mail.ru>
2022-10-25 16:04:53 +0500

    add days a week

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,17 +1,27 @@
 ﻿//Написать программу которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
 
-Console.Write("Введите первое число: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-if(numberB * numberB == numberA)
-{
-    Console.WriteLine("Да, является");
-}
-else
-{
-    Console.Write("Нет, не является");
-}
+//Console.Write("Введите первое число: ");
+//int numberA = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//int numberB = Convert.ToInt32(Console.ReadLine());
+//if(numberB * numberB == numberA)
+//{
+//    Console.WriteLine("Да, является");
+//}
+//else
+//{
+//    Console.Write("Нет, не является");
+//}
 
+// Написать программу которая будет выдавать название дня недели по заданному номеру
 
+Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 1) Console.Write("Понедельник");
+if(number == 2) Console.Write("Вторник");
+if(number == 3) Console.Write("Среда");
+if(number == 4) Console.Write("Четверг");
+if(number == 5) Console.Write("Пятница");
+if(number == 6) Console.Write("Суббота");
+if(number == 7) Console.Write("Воскресенье");
 
